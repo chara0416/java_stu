5fea2062a7ee37dcaa78232d63f5f7e90b9c9077
chara0416 <leo8689123@naver.com>
2025-04-10 11:28:01 +0900

    Update WinImage.java
    이미지 같은 크기로 정렬하기

--- WinImage.java
+++ WinImage.java
@@ -14,31 +14,31 @@ public class WinImage {
 		ImageIcon CatIcon = new ImageIcon("Img/cat.png");
 		ImageIcon DogIcon = new ImageIcon("Img/dog.jpg");
 		
-		JLabel lb1 = new JLabel();
-		lb1.setBounds(10, 10, 218, 231);
-		lb1.setIcon(CatIcon);
-        fr.add(lb1);
-        
-        Image catImg = CatIcon.getImage();
-        Image resizeCatImg = catImg.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+		Image catImg = CatIcon.getImage();
+        Image resizeCatImg = catImg.getScaledInstance(220, 230, Image.SCALE_SMOOTH);
         ImageIcon rCatIcon = new ImageIcon(resizeCatImg);
         
+		JLabel lb1 = new JLabel(rCatIcon);
+		lb1.setBounds(10, 10, 220, 230);
+		//lb1.setIcon(CatIcon);
+        fr.add(lb1);
+        
         JLabel lb2 = new JLabel(rCatIcon);
-		lb2.setBounds(240, 10, 100, 100);
+		lb2.setBounds(240, 10, 220, 230);
 		//lb2.setIcon(CatIcon);
         fr.add(lb2);
         
-        JLabel lb3 = new JLabel();
-		lb3.setBounds(10, 250, 225, 225);
-		lb3.setIcon(DogIcon);
-        fr.add(lb3);
-        
         Image dogImg = DogIcon.getImage();
-        Image resizeDogImg = dogImg.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        Image resizeDogImg = dogImg.getScaledInstance(220, 230, Image.SCALE_SMOOTH);
         ImageIcon rDogIcon = new ImageIcon(resizeDogImg);
         
+        JLabel lb3 = new JLabel(rDogIcon);
+		lb3.setBounds(10, 250, 220, 230);
+		//lb3.setIcon(DogIcon);
+        fr.add(lb3);
+        
         JLabel lb4 = new JLabel(rDogIcon);
-		lb4.setBounds(240, 250, 100, 100);
+		lb4.setBounds(240, 250, 220, 230);
 		//lb4.setIcon(DogIcon);
         fr.add(lb4);
         
